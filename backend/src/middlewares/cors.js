a66f5d3112a9fc33d953ed054ef87fe4caa73389
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-02-15 19:44:03 +0300

    where is my front

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -11,7 +11,7 @@ const allowedCors = [
 ];
 
 function cors(req, res, next) {
-  const { origin } = req.headers;
+  // const { origin } = req.headers;
   // if (allowedCors.includes(origin)) {
   //   res.header('Access-Control-Allow-Origin', '*');
   // }
