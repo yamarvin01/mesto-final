e36435b4ce6cd50d3671ee089181bd94baffde4a
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-02-15 14:55:23 +0300

    changes

--- backend/src/controllers/cards.js
+++ backend/src/controllers/cards.js
@@ -13,7 +13,7 @@ const createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
-    .then((card) => res.send(card))
+    .then((card) => res.send({ card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError());
