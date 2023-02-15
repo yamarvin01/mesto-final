213023ab7362622b0e0bf3826d288d3a0467b55f
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-02-15 10:51:00 +0300

    changes

---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,41 @@
+const BASE_URL = "https://auth.nomoreparties.co";
+
+function checkResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  return Promise.reject(`Ошибка: ${response.status} ${response.statusText}`);
+}
+
+export const register = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then(response => checkResponse(response));
+};
+
+export const authorize = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then(response => checkResponse(response));
+};
+
+export const getContent = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(response => checkResponse(response));
+};
