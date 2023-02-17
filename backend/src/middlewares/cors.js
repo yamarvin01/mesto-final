896bc4e92a0d966ff23844f26709aac2e348de5c
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-02-17 20:16:39 +0300

    changes

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,21 +1,17 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'localhost:4000',
+  'http://localhost:3000',
   'http://yamarvin01.nomoredomains.club',
   'https://yamarvin01.nomoredomains.club',
-  'yamarvin01.nomoredomains.club',
-  'www.yamarvin01.nomoredomains.club',
+  'http://api.yamarvin01.nomoredomainsclub.ru',
+  'https://api.yamarvin01.nomoredomainsclub.ru',
 ];
 
 function cors(req, res, next) {
-  // const { origin } = req.headers;
-  // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  // }
-  res.header('Access-Control-Allow-Origin', '*');
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
