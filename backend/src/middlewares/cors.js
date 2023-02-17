017fa2ed337b2f0a5511489352711d0de9f5ff04
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-02-17 20:32:58 +0300

    changes

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -17,12 +17,14 @@ function cors(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
+  // Указанным хостам разрешен любой метод
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
   }
 
+  // Указанным хостам разрешен любой заголовок
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
