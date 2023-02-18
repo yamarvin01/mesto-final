2d1e5df24ceb4532ab7aa3b83c719223caa3739f
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-02-18 14:43:46 +0300

    changes

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const allowedCors = [
   // Предполагается вести разработку под защищенным VPN
   'http://localhost:3000',
@@ -17,17 +18,13 @@ function cors(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  // Указанным хостам разрешен любой метод
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
-
-  // Указанным хостам разрешен любой заголовок
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
 
   next();
